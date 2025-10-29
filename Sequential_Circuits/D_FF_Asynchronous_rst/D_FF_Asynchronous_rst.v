0e8968a4534f1cb3707780fc2cb7a16db05da78b
Toshiv-Kumar <shadow.ptk@tutamail.com>
2025-10-29 23:47:47 +0530

    Create D_FF_Asynchronous_rst.v

---
+++ Sequential_Circuits/D_FF_Asynchronous_rst/D_FF_Asynchronous_rst.v
@@ -0,0 +1,13 @@
+module DFlipFlop_AsyncReset(D,clk,sync_reset,Q);
+input D; 
+input clk; 
+input async_reset; 
+output reg Q;  
+  always @(posedge clk or posedge async_reset) 
+begin
+  if(async_reset==1'b1)
+  Q <= 1'b0; 
+ else 
+  Q <= D; 
+end 
+endmodule 
